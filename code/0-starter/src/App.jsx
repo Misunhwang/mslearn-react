d00565789e2d9ed916f66153e1e04c998462d0d8
Sunny Hwang <anabelly1004@gmail.com>
2025-04-21 10:13:32 +1200

    React exercise

---
+++ code/0-starter/src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import RecipeTitle from "./RecipeTitle";
+
+function App() {
+  return (
+    <article>
+      <h1>Recipe Manager</h1>
+      <RecipeTitle />
+    </article>
+  );
+}
+
+export default App;
+// This is a simple React component that renders an article with a heading "Recipe Manager".
+// It uses the React library to create a functional component called App.
